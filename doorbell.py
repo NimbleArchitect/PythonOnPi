a9bb3f82266933403bff47f0ab13b62214982f29
NimbleArchitect <56797391+NimbleArchitect@users.noreply.github.com>
2019-10-26 11:41:39 +0100

    Create doorbell.py

---
+++ doorbell.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import requests
+import RPi.GPIO as GPIO
+
+input_pin = 17
+url = 'http://example.com/api/doorbell'
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(input_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+def button_press(channel):
+  state = GPIO.input(channel)
+  if state == 1:
+    req = requests.post(url, data = "on", headers = {"Content-type": "text/plain", "Accept-Encoding":""})
+  return
+
+
+GPIO.add_event_detect(input_pin, GPIO.BOTH, callback = button_press, bouncetime = 1)
+try:
+  while True:
+    sleep(1);
+
+finally:
+  GPIO.cleanup()
